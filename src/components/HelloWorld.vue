b89f3913148c6d950d07f6bd17a59dbb374be986
hml745655 <hml745655@gmail.com>
2025-04-14 03:32:17 +0000

    引入tailwindcss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,7 @@ console.log(dayjs().format('YYYY-MM-DD'));
 
 <h1>Hello App!</h1>
   <p>
-    <strong>Current route path:</strong> {{ $route.fullPath }}
+    <strong class="bg-red-900 underline">Current route path:</strong> {{ $route.fullPath }}
   </p>
   <nav>
     <RouterLink to="/">Go to Home </RouterLink>
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import App from './App.vue'
 import Antd from 'ant-design-vue';
 import router from './router/router'
 import 'ant-design-vue/dist/reset.css';
+import './style/index.css'
 
 createApp(App).use(Antd).use(router).mount('#app')
